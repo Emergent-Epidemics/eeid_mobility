bf4cf2c2dd6467dacf0894294d29d5e8b2aaa99d
Sam <sscarpin@gmail.com>
2022-06-05 12:00:50 -0400

    updating code to reflect dir structure

--- code/Baidu_analysis.R
+++ code/Baidu_analysis.R
@@ -31,8 +31,8 @@ make_mob <- function(f){
 ######
 #Data#
 ######
-files_in <- list.files("BaiduMobility/inbound/all", full.names = TRUE)
-files_out <- list.files("BaiduMobility/outbound/all", full.names = TRUE)
+files_in <- list.files("../data/BaiduMobility/inbound/all", full.names = TRUE)
+files_out <- list.files("../data/BaiduMobility/outbound/all", full.names = TRUE)
 
 dates_list_in <- lapply(X = files_in, FUN = get_dates, pat1 = "BaiduNet_in_")
 dates_in <- unlist(dates_list_in)
@@ -41,6 +41,9 @@ dates_in <- strptime(dates_in, format = "%Y%m%d")
 dates_list_out <- lapply(X = files_out, FUN = get_dates, pat1 = "BaiduNet_out_")
 dates_out <- unlist(dates_list_out)
 dates_out <- strptime(dates_out, format = "%Y%m%d")
+
+files_in <- files_in[order(dates_in)] #making sure the files are in the same order
+files_out <- files_in[order(dates_out)]
 
 ##########
 #Analysis#
@@ -51,26 +54,23 @@ wuhan_mob <- rep(NA, length(files_in))
 nanjing_mob <- rep(NA, length(files_in))
 shanghai_mob <- rep(NA, length(files_in))
 wu_sha <- rep(NA, length(files_in))
-counter <- 1
 
-files_in <- files_in[order(dates_in)] #making sure the files are in the same order
-files_out <- files_in[order(dates_out)]
-
+pb <- txtProgressBar(1, length(files_out), style=3)
 for(f in 1:length(files_out)){
   mob.in.f <- make_mob(files_in[f])
   mob.out.f <- make_mob(files_out[f])
   mob2 <- mob.in.f - t(mob.out.f)
   svs <- svd(mob2)
-  svds[counter] <- svs$d[1]
+  svds[f] <- svs$d[1]
   
   log_mobs <- log(mob2)
   log_mobs[which(is.finite(log_mobs) == FALSE)] <- NA
-  total_mob[counter] <- sum(log_mobs, na.rm = T)
-  wuhan_mob[counter] <- sum(log_mobs[which(row.names(mob2) == "武汉市"),], na.rm = T) #wuhan
-  nanjing_mob[counter] <- sum(log_mobs[which(row.names(mob2) == "南京市"),], na.rm = T) #nanjing
-  shanghai_mob[counter] <- sum(log_mobs[which(row.names(mob2) == "上海市"),], na.rm = T) #shanghair
-  wu_sha[counter] <- log_mobs[which(row.names(mob2) == "武汉市"),which(row.names(mob2) == "上海市")]
-  counter <- counter + 1
+  total_mob[f] <- sum(log_mobs, na.rm = T)
+  wuhan_mob[f] <- sum(log_mobs[which(row.names(mob2) == "武汉市"),], na.rm = T) #wuhan
+  nanjing_mob[f] <- sum(log_mobs[which(row.names(mob2) == "南京市"),], na.rm = T) #nanjing
+  shanghai_mob[f] <- sum(log_mobs[which(row.names(mob2) == "上海市"),], na.rm = T) #shanghair
+  wu_sha[f] <- log_mobs[which(row.names(mob2) == "武汉市"),which(row.names(mob2) == "上海市")]
+  setTxtProgressBar(pb, f)
 }
 dates <- dates_in
 
@@ -79,4 +79,4 @@ plot(dates, total_mob, type = "l")
 plot(dates, wuhan_mob, type = "l")
 plot(dates, nanjing_mob, type = "l")
 plot(dates, shanghai_mob, type = "l")
-plot(dates, wu_sha, type = "l")
+plot(dates, wu_sha, type = "l", xlab = "2020", ylab = "Relative mobility", bty = "n", lwd = 3)
